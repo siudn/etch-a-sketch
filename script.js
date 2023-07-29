12717d3798a89622da26ac93b3b05914fb85385b
siudn <cshadx@gmail.com>
2023-07-28 23:33:29 -0400

    16 x 16 grid can be colored in

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const container = document.querySelector(".grid");
+const grid = document.querySelector(".grid");
 
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         var square = document.createElement("div");
         square.classList.add("square");
-        container.append(square);
+        grid.append(square);
     }
 }
+
+const elements = document.getElementsByClassName("square");
+
+Array.from(elements).forEach((i) => {
+    i.addEventListener("mouseover", () => {
+        i.classList.add("colored");
+    })
+})
+
